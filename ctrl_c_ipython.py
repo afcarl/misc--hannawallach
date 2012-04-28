85ddb623a879159ba2fb8f9b6e1aeee093006eb7
Hanna Wallach <wallach@cs.umass.edu>
2012-04-28 00:54:39 -0400

    Added code to test catching a KeyboardInterrupt and, when caught, embedding (i.e., instantiating) an ipython interactive shell.

---
+++ ctrl_c_ipython.py
@@ -0,0 +1,15 @@
+from IPython import embed
+from time import sleep
+
+def main():
+
+    x = ['hello', 'world']
+
+    for i in xrange(1000000000):
+        try:
+            sleep
+        except KeyboardInterrupt:
+            embed()
+
+if __name__ == '__main__':
+    main()
